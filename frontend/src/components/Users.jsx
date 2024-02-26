6ebfb1bb622573890b0928b47de051a5398bc0ea
Vedansh Mishra <122162739+vedansh2001@users.noreply.github.com>
2024-02-26 20:20:32 +0530

    Update the backend link in dashboard/user 4th time

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -10,7 +10,7 @@ export const Users = () => {
     const [filter, setFilter] = useState("");
 
     useEffect(() => {
-        axios.get("https://paytm-app-rose.vercel.app//api/v1/user/bulk?filter=" + filter)
+        axios.get("https://paytm-app-rose.vercel.app/api/v1/user/bulk?filter=" + filter)
             .then(response => {
                 setUsers(response.data.user)
             })
